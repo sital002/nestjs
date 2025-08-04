35c44de575d5c7a088cbab53014be09aad585aa9
sital002 <sitaladhikari002@gmail.com>
2025-08-04 13:09:22 +0545

    removed prettier

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,17 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get } from "@nestjs/common";
+import { AppService } from "./app.service";
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) { }
+  constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
 
-  @Get('/test')
+  @Get("/test")
   test(): string {
-    return 'This is a test page';
+    return "This is a test page";
   }
 }
