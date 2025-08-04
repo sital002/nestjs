b4dc5803ba694468330cca5d4ac220e1ce723631
sital002 <sitaladhikari002@gmail.com>
2025-08-04 14:14:36 +0545

    feat: guard in nestjs

--- src/test-page/test.controller.ts
+++ src/test-page/test.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, Request, UseGuards } from "@nestjs/common";
 import { TestService } from "./test.service";
+import { AuthGuard } from "src/auth/auth.guard";
 
 @Controller("test")
 export class TestController {
   constructor(private readonly testServie: TestService) {}
 
+  @UseGuards(AuthGuard)
   @Get("/test1")
-  test(): string {
+  test(@Request() req): string {
+    console.log(req["admin"]);
     return this.testServie.getTest();
   }
 }
